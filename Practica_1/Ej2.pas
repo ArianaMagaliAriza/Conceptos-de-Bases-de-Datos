3ccae7bbd3d039ce2b4009d7415ce7a9bf208512
ArianaMagaliAriza <arianamagaliariza@gmail.com>
2025-03-14 01:32:09 -0300

    porfavorfunciona

---
+++ Practica_1/Ej2.pas
@@ -0,0 +1,6 @@
+{2. Desarrollar un programa que permita la apertura de un archivo de números enteros no ordenados.
+ La información del archivo corresponde a la cantidad de votantes de cada ciudad de la Provincia 
+ de Buenos Aires en una elección presidencial. Recorriendo el archivo una única vez, informe por pantalla
+ la cantidad mínima y máxima de votantes. Además durante el recorrido, el programa deberá listar el contenido
+ del archivo en pantalla.
+ El nombre del archivo a procesar debe ser proporcionado por el usuario.}
